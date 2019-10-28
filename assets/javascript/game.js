abef4286985693075a517b151d10608ec463161c
Kassandra Gonzalez <kgkassieg@gmail.com>
2019-10-28 18:53:41 -0500

    created new functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,67 @@
+// Global variables for game state
+
+console.log("HEY!");
+
 var targetNumber = 0;
 var wins = 0;
 var losses = 0;
 var totalScoreNumber = 0;
+var gem1Value;
+var gem2Value;
+var gem3Value;
+var gem4Value;
 
-var gem1 = Math.floor(Math.random() * 11) + 1;
-var gem2 = Math.floor(Math.random() * 11) + 1;
-var gem3 = Math.floor(Math.random() * 11) + 1;
-var gem4 = Math.floor(Math.random() * 11) + 1;
-var targetNumber = Math.floor(Math.random() * 120) + 19;
 
+
+
+// Gem elements
+var gemOne = $(".gem1");
+
+// Set values function
+var setValues = function() {
+  // Random values for gems
+  gem1Value = Math.floor(Math.random() * 11) + 1;
+  gem2Value = Math.floor(Math.random() * 11) + 1;
+  gem3Value = Math.floor(Math.random() * 11) + 1;
+  gem4Value = Math.floor(Math.random() * 11) + 1;
+  targetNumber = Math.floor(Math.random() * 120) + 19;
+
+}
+
+
+
+// Function runs once document is loaded
 $(document).ready(function() {
+  //
   var startGame = function() {
+    setValues()
     var score = 0;
+    gemOne.attr("value", gem1Value);
+    console.log(gemOne);
 
-    targetNumber = Math.floor(Math.random() * 120) + 19;
-    console.log(targetNumber);
-    gem1.value = Math.floor(Math.random() * 12) + 1;
-    console.log(gem1);
-    gem2.value = Math.floor(Math.random() * 12) + 1;
-    console.log(gem2);
-    gem3.value = Math.floor(Math.random() * 12) + 1;
-    console.log(gem3);
-    gem4.value = Math.floor(Math.random() * 12) + 1;
-    console.log(gem4);
-    $("#targetNumber").html(targetNumber);
-    $("#score").html(score);
+    // targetNumber = Math.floor(Math.random() * 120) + 19;
+    // console.log(targetNumber);
+    // gem1.value = Math.floor(Math.random() * 12) + 1;
+    // console.log(gem1);
+    // gem2.value = Math.floor(Math.random() * 12) + 1;
+    // console.log(gem2);
+    // gem3.value = Math.floor(Math.random() * 12) + 1;
+    // console.log(gem3);
+    // gem4.value = Math.floor(Math.random() * 12) + 1;
+    // console.log(gem4);
+    // $("#targetNumber").html(targetNumber);
+    // $("#score").html(score);
   };
 
-  resetGame();
+  // Set game values
 
-  $(".gem1").click(function() {
+  // Listen for click on element
+
+  // Get value for clicked element
+
+  // resetGame();
+
+  gemOne.click(function() {
     console.log(".score");
   });
   $(".gem2").click(function() {
@@ -53,4 +85,6 @@ $(document).ready(function() {
     losses++;
     $(".loss").text("Losses: " + losses);
   }
+
+  startGame();
 });
